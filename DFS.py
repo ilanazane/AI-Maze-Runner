b90c3730596ebcb8513d25e0b96b5601626ddb65
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-08 23:22:11 -0500

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -2,7 +2,7 @@ from methods import *
 
 #DFS Search Algorithm
 
-def DFS(maze):
+def DFS(maze, video):
     #initialize the solved state of the maze to be false and our pointers to be at the beginning
     #i controls row and j controls column
     maze_final = np.copy(maze)
@@ -31,9 +31,13 @@ def DFS(maze):
         if len(fringe) == 0:
             #update the state of the maze, display the end result, and break the loop
             update(maze, i , j)
-            plt.imshow(maze, cmap=plt.cm.binary)
-            plt.show()
+            if video == True:
+                plt.imshow(maze, cmap=plt.cm.binary)
+                plt.show()
             print("UNSOLVABLE")
+            #value to be returned for later analysis
+            solved = 0
+            solution_length = 0
             break
             
         #gets the current node and update i and j
@@ -43,24 +47,33 @@ def DFS(maze):
         #check if we have reached a solution, display the end result, and break the loop
         if i + 1 == len(maze) and j + 1 == len(maze[i]):
             update(maze, i , j)
-            plt.imshow(maze, cmap=plt.cm.binary)
-            plt.pause(0.05)
-            print("SOLVED")
+            if video == True:
+                plt.imshow(maze, cmap=plt.cm.binary)
+                plt.pause(0.05)
+            print("SOLUTION FOUND")
+            #value to be returned for later analysis
+            solved = 1
+            
+            solution_length = 0
             
             update(maze_final, i, j)
-            plt.imshow(maze, cmap=plt.cm.binary)
-            plt.pause(0.05)
+            if video == True:
+                plt.imshow(maze, cmap=plt.cm.binary)
+                plt.pause(0.05)
             
             while i != 0 or j!= 0:
                 x = prev[i,j]
                 i, j = x[0], x[1]
                 update(maze_final, i, j)
-                plt.imshow(maze_final, cmap=plt.cm.binary)
-                plt.pause(0.05)
+                if video == True:
+                    plt.imshow(maze_final, cmap=plt.cm.binary)
+                    plt.pause(0.05)
+                solution_length += 1
             
             update(maze_final, 0, 0)
-            plt.imshow(maze_final, cmap=plt.cm.binary)
-            plt.pause(0.05)
+            if video == True:
+                plt.imshow(maze_final, cmap=plt.cm.binary)
+                plt.pause(0.05)
             
             break
             
@@ -137,9 +150,13 @@ def DFS(maze):
         #after done checking, update the maze and start over
         update(maze, i, j)
         
-        plt.imshow(maze, cmap=plt.cm.binary)
-        plt.pause(0.05)
+        if video == True:
+            plt.imshow(maze, cmap=plt.cm.binary)
+            plt.pause(0.05)
 
-    plt.show()
+    if video == True:
+        plt.show()
+        
+    return solved, solution_length
     
-DFS(grid(10, 0.2))
+x, y = DFS(grid(10, 0.2), video = False)
