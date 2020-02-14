c79a7e6235207481415389e0c92cd3b13bbb9d24
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-14 00:00:47 -0500

    Update BiBFS.py

--- BiBFS.py
+++ BiBFS.py
@@ -1,17 +1,32 @@
 from methods import *
-#Bi-Directional BFS
+
+'''
+BiBFS Search Algorithm
+
+            **inputs**
+
+-maze = a dim x dim array to be worked with
+-video = boolean variable to either show a live update of the maze or not
+
+            **returns**
+            
+-solved = 1 if solved, 0 if not
+-solution_length = integer value of the final solution length
+'''
 
 def BiBFS(maze, video):
     #initialized state is set to false
     #i,j-->row,column from start m,n-->row,column from end
-    maze_final=np.copy(maze)
     solved=False
     i,j=0,0
     m,n=len(maze)-1,len(maze[0])-1
+    #make a copy of the maze to be used to reconstruct the final path once a solution is found
+    maze_final=np.copy(maze)
+    #create two dictionaries, one to backtrack one way, one to backtrack the other way
     prev={}
     prev2={}
     
-    #initialize fringe for starting point and fringe from ending point
+    #initialize fringe for starting point and fringe for ending point
     fringeStart=queue.Queue()
     fringeStart.put([i,j])
     fringeEnd=queue.Queue()
@@ -28,7 +43,7 @@ def BiBFS(maze, video):
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.pause(0.05)
             print("UNSOLVABLE")
-            #value to be returned for later analysis
+            #set the values of solved and solution_length to be zero
             solved = 0
             solution_length = 0
             break
@@ -39,7 +54,7 @@ def BiBFS(maze, video):
         i,j=currentStart[0],currentStart[1]
         m,n=currentEnd[0],currentEnd[1]
         
-        #check if start and end meet in middle,display result, then break loop
+        #check if start and end meet in middle, if so, display result and break loop
         if (i+1,j) in prev2:
             m, n = i+1, j
             solved = True
@@ -63,10 +78,11 @@ def BiBFS(maze, video):
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.pause(0.05)
             print("SOLUTION FOUND")
-            #value to be returned for later analysis
+            #set the values of solved to one and initialize solution_length
             solved = 1
             solution_length = 1
             
+            #start to reconstruct the path back and increment solution_length
             update(maze_final,i,j)
             update(maze_final,m,n)
             if video == True:
@@ -77,6 +93,8 @@ def BiBFS(maze, video):
                 if i==0 and j==0 and m==len(maze)-1 and n==len(maze[0])-1:
                     break
 
+                #Conditions to make sure maze reconstructs properly
+                
                 if (i,j) == (0,0):
                     y=prev2[(m,n)]
                     m,n=y[0],y[1]
@@ -154,7 +172,7 @@ def BiBFS(maze, video):
         if j + 1 >= len(maze[i]):
             pass
         else:
-            #is the next position occupied or iously visited?
+            #is the next position occupied or previously visited?
             if maze[i][j + 1] == 1 or maze[i][j + 1] == 0.5:
                 pass
             else:
@@ -240,7 +258,7 @@ def BiBFS(maze, video):
                     prev2[(m, n + 1)] = (m, n)
                     fringeEnd.put([m, n + 1])
                     
-        #after done checking, update maze and start over
+        #after done checking, update maze and keep going
         update(maze, i, j)
         update(maze,m,n)
         if video == True:
@@ -248,6 +266,13 @@ def BiBFS(maze, video):
             plt.pause(0.05)
     if video == True:        
         plt.show()
-    return solved, solution_length
     
-x, y = BiBFS(grid(10,0.2), video = False)
+    #show the final state of the maze whether video is True of False
+    plt.figure(figsize=(15,15))
+    plt.title("BiBFS", fontsize = 40)
+    plt.imshow(maze_final, cmap=plt.cm.binary)
+    plt.show()
+    
+    return solved, solution_length
+
+#x, y = BiBFS(grid(100, 0.3), video = False) <----- example of how to run BiBFS
