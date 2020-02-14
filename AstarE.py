a06007f471cda5b902bad4b97173369582d71313
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-14 10:29:50 -0500

    Update AstarE.py

--- AstarE.py
+++ AstarE.py
@@ -13,7 +13,7 @@ A* Euclidean Search Algorithm
 -solved = 1 if solved, 0 if not
 -solution_length = integer value of the final solution length
 
-x, y, z = AstarE(grid(100, 0.3), video = False) #<----- AstarE Example
+x, y = AstarE(grid(100, 0.3), video = False) #<----- AstarE Example
 '''
 
 def AstarE(maze, video):
