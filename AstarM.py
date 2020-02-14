590e3470e1d8ed82a44d0b5f1b30855276a5cf98
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-14 00:20:46 -0500

    Update AstarM.py

--- AstarM.py
+++ AstarM.py
@@ -1,34 +1,44 @@
 from methods import *
 
-#A* Manhattan Distance
+'''
+A* Manhattan Search Algorithm
+
+            **inputs**
+
+-maze = a dim x dim array to be worked with
+-video = boolean variable to either show a live update of the maze or not
+
+            **returns**
+            
+-solved = 1 if solved, 0 if not
+-solution_length = integer value of the final solution length
+-maxNode = the total number of nodes expanded, to be used for local search
+'''
 
 def AstarM(maze, video):
     #initialize the solved state of the maze to be false and our pointers to be at the beginning
     #i controls row and j controls column
-    maze_final = np.copy(maze)
     solved = False
     i, j = 0, 0
+    #make a copy of the maze to be used to reconstruct the final path once a solution is found
+    maze_final = np.copy(maze)
+    #create a dictionary called prev to point to previous positions
     prev = {}
+    #establish a counter to keep track of the number of moves made so far (is g(n))
     counter = 0
+    #all nodes explored. initialize to 0 
+    maxNode=0
     
     #initialize the fringe and store the starting point of the maze
     fringe = []
-    fringe.append([i, j, counter + Manhattan(maze, i, j)])
+    fringe.append((i, j, counter + Manhattan(maze, i, j)))
     
-    
+    if video == True:
+        plt.imshow(maze, cmap=plt.cm.binary)
+        plt.pause(0.05)
+        
     #runs until we reach the end
     while solved == False:
-        '''
-        #*****this is just for debugging*****
-        
-        #print out the length of the current fringe
-        print(queue.Queue.qsize(fringe))
-            
-        #looks at the queue
-        for q_item in fringe.queue:
-            print(q_item)
-        '''
-        
         #Is the maze unsolvable?
         if len(fringe) == 0:
             #update the state of the maze, display the end result, and break the loop
@@ -37,35 +47,39 @@ def AstarM(maze, video):
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.show()
             print("UNSOLVABLE")
-            #value to be returned for later analysis
+            #set the values of solved and solution_length to be zero
             solved = 0
             solution_length = 0
             break
             
-        #gets the current node and update i and j
+        #gets the current node and update i and j; update counter
         current = fringe.pop()
+        #every time a node is explored, increase maxNode by 1 
+        maxNode += 1
         i, j = current[0], current[1]
+        counter = current[2] - Manhattan(maze, i, j) + 1
         
-        #check if we have reached a solution, display the end result, and break the loop
+        #check if we have reached a solution, if so, display the end result and break the loop
         if i + 1 == len(maze) and j + 1 == len(maze[i]):
+            moves = current[2]
             update(maze, i , j)
             if video == True:
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.pause(0.05)
             print("SOLUTION FOUND")
-            #value to be returned for later analysis
+            #set the values of solved to one and initialize solution_length to be zero
             solved = 1
-            
             solution_length = 0
             
+            #start to reconstruct the path back and increment solution_length
             update(maze_final, i, j)
             if video == True:
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.pause(0.05)
             
             while i != 0 or j!= 0:
-                x = prev[i,j]
-                i, j = x[0], x[1]
+                x = prev[i,j, moves]
+                i, j, moves = x[0], x[1], x[2]
                 update(maze_final, i, j)
                 if video == True:
                     plt.imshow(maze_final, cmap=plt.cm.binary)
@@ -82,6 +96,7 @@ def AstarM(maze, video):
         #check left solution
         
         #are we outside?
+        
         if j - 1 < 0:
             pass
         else:
@@ -91,12 +106,12 @@ def AstarM(maze, video):
                 pass
             else:
                 #check if already in fringe
-                if [i, j - 1, counter + Manhattan(maze, i, j - 1)] in fringe:
+                if (i, j - 1, counter + Manhattan(maze, i, j - 1)) in fringe:
                     pass
                 else:
                     #if the fringe was empty (i.e. the first move), just add the first child to fringe
                     if len(fringe) == 0: 
-                        prev[(i, j - 1)] = (i, j)
+                        prev[(i, j - 1, counter + Manhattan(maze, i, j - 1))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                         fringe.append((i, j - 1, counter + Manhattan(maze, i, j - 1)))
                     #if the fringe is not empty, check the f(n) value and compare with items in fringe
                     else:
@@ -104,14 +119,14 @@ def AstarM(maze, video):
                             if counter + Manhattan(maze, i, j - 1) > fringe[x][2]:
                                 if x == 0:
                                     fringe.insert(0, (i, j - 1, counter + Manhattan(maze, i, j - 1)))
-                                    prev[(i, j - 1)] = (i, j)
+                                    prev[(i, j - 1, counter + Manhattan(maze, i, j - 1))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 else:
                                     pass
                             else: 
                                 fringe.insert(x + 1, (i, j - 1, counter + Manhattan(maze, i, j - 1)))
-                                prev[(i, j - 1)] = (i, j)
+                                prev[(i, j - 1, counter + Manhattan(maze, i, j - 1))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 break
-                                
+        
         #check up solution
         
         #are we outside?
@@ -123,12 +138,12 @@ def AstarM(maze, video):
                 pass
             else:
                 #check if already in fringe
-                if [i - 1, j, counter + Manhattan(maze, i - 1, j)] in fringe:
+                if (i - 1, j, counter + Manhattan(maze, i - 1, j)) in fringe:
                     pass
                 else:
                     #if the fringe was empty (i.e. the first move), just add the first child to fringe
                     if len(fringe) == 0: 
-                        prev[(i - 1, j)] = (i, j)
+                        prev[(i - 1, j, counter + Manhattan(maze, i - 1, j))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                         fringe.append((i - 1, j, counter + Manhattan(maze, i - 1, j)))
                     #if the fringe is not empty, check the f(n) value and compare with items in fringe
                     else:
@@ -136,13 +151,14 @@ def AstarM(maze, video):
                             if counter + Manhattan(maze, i - 1, j) > fringe[x][2]:
                                 if x == 0:
                                     fringe.insert(0, (i - 1, j, counter + Manhattan(maze, i - 1, j)))
-                                    prev[(i - 1, j)] = (i, j)
+                                    prev[(i - 1, j, counter + Manhattan(maze, i - 1, j))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 else: 
                                     pass
                             else: 
                                 fringe.insert(x + 1, (i - 1, j, counter + Manhattan(maze, i - 1, j)))
-                                prev[(i - 1, j)] = (i, j)
+                                prev[(i - 1, j, counter + Manhattan(maze, i - 1, j))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 break
+                                
         #check right position
         
         #are we outside?
@@ -154,12 +170,12 @@ def AstarM(maze, video):
                 pass
             else:
                 #check if already in fringe
-                if [i, j + 1, counter + Manhattan(maze, i, j + 1)] in fringe:
+                if (i, j + 1, counter + Manhattan(maze, i, j + 1)) in fringe:
                     pass
                 else:
                     #if the fringe was empty (i.e. the first move), just add the first child to fringe
                     if len(fringe) == 0: 
-                        prev[(i, j + 1)] = (i, j)
+                        prev[(i, j + 1, counter + Manhattan(maze, i, j + 1))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                         fringe.append((i, j + 1, counter + Manhattan(maze, i, j + 1)))
                     #if the fringe is not empty, check the f(n) value and compare with items in fringe
                     else:
@@ -167,16 +183,14 @@ def AstarM(maze, video):
                             if counter + Manhattan(maze, i, j + 1) > fringe[x][2]:
                                 if x == 0:
                                     fringe.insert(0, (i, j + 1, counter + Manhattan(maze, i, j + 1)))
-                                    prev[(i, j + 1)] = (i, j)
+                                    prev[(i, j + 1, counter + Manhattan(maze, i, j + 1))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 else: 
                                     pass
                             else: 
                                 fringe.insert(x + 1, (i, j + 1, counter + Manhattan(maze, i, j + 1)))
-                                prev[(i, j + 1)] = (i, j)
+                                prev[(i, j + 1, counter + Manhattan(maze, i, j + 1))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 break
                                 
-
-        
         #check down position
         
         #are we outside?
@@ -188,12 +202,12 @@ def AstarM(maze, video):
                 pass
             else:
                 #check if already in fringe
-                if [i + 1, j, counter + Manhattan(maze, i + 1, j)] in fringe:
+                if (i + 1, j, counter + Manhattan(maze, i + 1, j)) in fringe:
                     pass
                 else:
                     #if the fringe was empty (i.e. the first move), just add the first child to fringe
                     if len(fringe) == 0: 
-                        prev[(i + 1, j)] = (i, j)
+                        prev[(i + 1, j, counter + Manhattan(maze, i + 1, j))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                         fringe.append((i + 1, j, counter + Manhattan(maze, i + 1, j)))
                     #if the fringe is not empty, check the f(n) value and compare with items in fringe
                     else:
@@ -201,25 +215,28 @@ def AstarM(maze, video):
                             if counter + Manhattan(maze, i + 1, j) > fringe[x][2]:
                                 if x == 0:
                                     fringe.insert(0, (i + 1, j, counter + Manhattan(maze, i + 1, j)))
-                                    prev[(i + 1, j)] = (i, j)
+                                    prev[(i + 1, j, counter + Manhattan(maze, i + 1, j))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 else: 
                                     pass
                             else: 
                                 fringe.insert(x + 1, (i + 1, j, counter + Manhattan(maze, i + 1, j)))
-                                prev[(i + 1, j)] = (i, j)
+                                prev[(i + 1, j, counter + Manhattan(maze, i + 1, j))] = (i, j, counter - 1 + Manhattan(maze, i, j))
                                 break        
         
-        
-        
-        #after done checking, update the maze and start over
+    
+        #after done checking, update the maze and keep going
         update(maze, i, j)
         
         if video == True:
             plt.imshow(maze, cmap=plt.cm.binary)
             plt.pause(0.05)
-    if video == True:
-        plt.show()
         
-    return solved, solution_length
-    
-x, y = AstarM(grid(10, 0.2), video = False)
+    #show the final state of the maze whether video is True of False
+    plt.figure(figsize=(15,15))
+    plt.title("AstarM", fontsize = 40)
+    plt.imshow(maze_final, cmap=plt.cm.binary)
+    plt.show()
+        
+    return solved, solution_length, maxNode
+
+#x, y, z = AstarM(grid(100, 0.3), video = False) <----- example of how to run AstarM
