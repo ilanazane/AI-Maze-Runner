cdc64d3cf54a7127cf948735fdfc9222d63eebe6
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-14 10:27:38 -0500

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -12,6 +12,8 @@ BFS Search Algorithm
             
 -solved = 1 if solved, 0 if not
 -solution_length = integer value of the final solution length
+
+x, y, z = BFS(grid(100, 0.3), video = False) #<----- BFS Example
 '''
 
 def BFS(maze, video):
