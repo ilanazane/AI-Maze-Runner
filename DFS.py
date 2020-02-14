e7b482ef032df3145a7f73e4a3772cf5890cb020
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-13 23:53:05 -0500

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,31 +1,47 @@
 from methods import *
 
-#DFS Search Algorithm
+'''
+DFS Search Algorithm
+
+            **inputs**
+
+-maze = a dim x dim array to be worked with
+-video = boolean variable to either show a live update of the maze or not
+
+            **returns**
+            
+-solved = 1 if solved, 0 if not
+-solution_length = integer value of the final solution length
+-maxf = maximum size of the fringe during the search, to be used for local search
+'''
 
 def DFS(maze, video):
     #initialize the solved state of the maze to be false and our pointers to be at the beginning
     #i controls row and j controls column
-    maze_final = np.copy(maze)
     solved = False
     i, j = 0, 0
+    #make a copy of the maze to be used to reconstruct the final path once a solution is found
+    maze_final = np.copy(maze)
+    #create a dictionary called prev to point to previous positions
     prev = {}
+    #largest size of fringe; initialize to 0 
+    maxf = 0 
     
-    #initialize the fringe and store the starting point of the maze
+    #initialize the fringe (a stack) and store the starting point of the maze
     fringe = []
-    fringe.append([i, j])
+    fringe.append((i, j))
+    
+    if video == True:
+        plt.imshow(maze, cmap=plt.cm.binary)
+        plt.pause(0.05)
     
     #runs until we reach the end
     while solved == False:
-        '''
-        #*****this is just for debugging*****
-        
-        #print out the length of the current fringe
-        print(queue.Queue.qsize(fringe))
-            
-        #looks at the queue
-        for q_item in fringe.queue:
-            print(q_item)
-        '''
+        #update the max length of the fringe
+        if len(fringe) > maxf: 
+            maxf = len(fringe)
+        else: 
+            pass
         
         #Is the maze unsolvable?
         if len(fringe) == 0:
@@ -35,7 +51,7 @@ def DFS(maze, video):
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.show()
             print("UNSOLVABLE")
-            #value to be returned for later analysis
+            #set the values of solved and solution_length to be zero
             solved = 0
             solution_length = 0
             break
@@ -44,18 +60,18 @@ def DFS(maze, video):
         current = fringe.pop()
         i, j = current[0], current[1]
         
-        #check if we have reached a solution, display the end result, and break the loop
+        #check if we have reached a solution, if so, display the end result and break the loop
         if i + 1 == len(maze) and j + 1 == len(maze[i]):
             update(maze, i , j)
             if video == True:
                 plt.imshow(maze, cmap=plt.cm.binary)
                 plt.pause(0.05)
             print("SOLUTION FOUND")
-            #value to be returned for later analysis
+            #set the values of solved to one and initialize solution_length to be zero
             solved = 1
-            
             solution_length = 0
             
+            #start to reconstruct the path back and increment solution_length
             update(maze_final, i, j)
             if video == True:
                 plt.imshow(maze, cmap=plt.cm.binary)
@@ -157,6 +173,11 @@ def DFS(maze, video):
     if video == True:
         plt.show()
         
-    return solved, solution_length
-    
-x, y = DFS(grid(10, 0.2), video = False)
+    plt.figure(figsize=(15,15))
+    plt.title("DFS", fontsize = 40)
+    plt.imshow(maze_final, cmap=plt.cm.binary)
+    plt.show()
+        
+    return solved, solution_length, maxf
+
+#x, y, z = DFS(grid(100, 0.3), video = False) <----- example of how to run DFS
