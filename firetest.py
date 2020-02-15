188ad3f0b1e10eb7bdb3c2f8f556cfc60a787008
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-15 16:15:01 -0500

    Update firetest.py

--- firetest.py
+++ firetest.py
@@ -1,10 +1,28 @@
 from methods import *
 from fire1 import fire1
 from fire2 import fire2
+from fire3 import fire3
+
+'''
+***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+For running the different strategies on the same mazes 
+-fire will still spread differently for each maze, but starting position is the same
+***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+'''
 
 #make copies of the same maze to be run by each algorithm
 a = firegrid(10, 0.2)
 b = np.copy(a)
+c = np.copy(a)
 
-fire1(a, video = True, show_final = True, q = 0.1)
-fire2(b, video = True, show_final = True, q = 0.1)
+print("Strategy 1: Regular A*Manhattan")
+time.sleep(2)
+fire1(a, video = True, show_final = False, q = 0.1)
+print()
+print("Strategy 2: Adapt to Environment")
+time.sleep(2)
+fire2(b, video = True, show_final = False, q = 0.1)
+print()
+print("Strategy 3: Plan Ahead")
+time.sleep(2)
+fire3(c, video = True, show_final = False, q = 0.1)
