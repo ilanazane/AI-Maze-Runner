2523022aaf7d455a37d9531ba8002fda8f6a8c71
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-15 01:40:31 -0500

    Update local_search_tester.py

--- Tester_Files/local_search_tester.py
+++ Tester_Files/local_search_tester.py
@@ -6,8 +6,31 @@ from Algorithms.DFS import DFS
 from Local_Search.hillClimbAstarM import hillClimbAstarM
 from Local_Search.hillClimbDFS import hillClimbDFS
 
-a = grid(75, 0.2)
-b = np.copy(a)
+'''
+***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+This file is showing off the local search algorithms
+Each hill climbing algorithm is run on the same mazes, and the before and afters are displayed
 
-harder_DFS_maze = hillClimbDFS(a, 0)
-harder_AstarM_maze = hillClimb(b, 0)
+hillClimbDFS will try to maximize the total fringe size
+hillCLimbAstarM will try to maximize the total nodes explored
+***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+'''
+
+a = grid(30, 0.2)
+b = np.copy(a)
+c = np.copy(a)
+d = np.copy(a)
+
+#show initial path returned by DFS
+DFS(a, video = False, show_final = True)
+#hill climb
+harder_DFS_maze = hillClimbDFS(b, 0)
+#show the final path returned by DFS
+DFS(harder_DFS_maze, video = False, show_final = True)
+
+#show initial path returned by AstarM
+AstarM(c, video = False, show_final = True)
+#hill climb
+harder_AstarM_maze = hillClimbAstarM(d, 0)
+#show the final path returned by AstarM
+AstarM(harder_AstarM_maze, video = False, show_final = True)
