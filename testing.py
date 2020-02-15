ab30199113d092976ee9e2418883e2b0aea71643
Will Bidle <33603804+WilliamBidle@users.noreply.github.com>
2020-02-14 21:49:50 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,6 +5,16 @@ from BiBFS import BiBFS
 from AstarE import AstarE
 from AstarM import AstarM
 
+'''
+***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+This file is purely for showing how the algorithms run via displaying the final path returned
+
+To show off the video update, we run BiBFS through a smaller grid (to prevent the computer from having a heart attack)
+
+Just change video = False to video = True for the below algorithms to see large grid video updates (Warning: will make computer wheeze a little)
+***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+'''
+
 #x, y = BFS(grid(100, 0.3), video = False) #<----- BFS Example
 #x, y, z = DFS(grid(100, 0.3), video = False) #<----- DFS Example
 #x, y = BiBFS(grid(100, 0.3), video = False) #<----- BiBFS Example
@@ -18,8 +28,11 @@ c = np.copy(a)
 d = np.copy(a)
 e = np.copy(a)
 
-BFS(a, video = False)
-DFS(b, video = False)
-BiBFS(c, video = False)
-AstarM(d, video = False)
-AstarE(e, video = False)
+BFS(a, video = False, show_final = True)
+DFS(b, video = False, show_final = True)
+BiBFS(c, video = False, show_final = True)
+AstarM(d, video = False, show_final = True)
+AstarE(e, video = False, show_final = True)
+
+#demonstrate the video for BiBFS (the others are all the same... this one is the coolest)
+BiBFS(grid(10, 0.2), video = True, show_final = True)
